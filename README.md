c77f2b3eae503ac0c203ef30ef4cc3f896c25ce0
Lawrence Smith <63609867+ArraysStartAt2@users.noreply.github.com>
2021-05-06 18:01:47 -0500

    Added the ability to filter platforms for both seed and layer subcommands. Also added a flag for not downloading subtechniques.

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # attackexcel
+
+---  
+
+A command-line tool to work with MITRE ATT&CK and Excel
+
+Attackexcel was born out of a necessity to create MITRE ATT&CK Heatmaps quickly and easily that
+represent a variety of things. It does this through the use of two sub-commands: *seed*, and *layer*.
+The former downloads the knowledgebase from the MITRE TAXII server into an Excel workbook and adds some
+useful columns and functions, while the latter transforms the workbook into a json file suitable for use
+with the ATT&CK Navigator.
+
+This tool works with version 9 of MITRE ATT&CK, version 4.3 of the navigator, and 4.2 of the layer files.
+## Sub-command: seed
+### Basic usage
+`attackexcel seed <filename>`
+
+This will download the Enterprise matrix into the named file, including all techniques and subtechniques
+for every platform. It will create the following individual worksheets in an attempt to normalize the data.
+
+### Working with a different matrix
+By default *seed* will download the Enterprise matrix. You can choose to download one of the other two matrices
+by using the --domain switch.
+
+This will download the ICS matrix:
+
+`attackexcel seed [filename] --domain ics-attack`
+
+This will download the Mobile matrix:
+
+`attackexcel seed [filename] --domain mobile-attack`
+
+
+### Filtering
+By default, *seed* will include techniques associated with all platforms (even PRE!), but you can change this
+behavior with the platformfilterin or platformfilterout switches and a space-separated list of platforms. If using
+platformfilterin, a given technique will be included if at least one of its associated platforms is in the list. If
+using platformfilterout, a given technique will be included only if *none* of its associated platforms are in the list.
+These switches are mutually exclusive. Filter values are case-sensitive, and must be one of 'SaaS', 'macOS', 'PRE',
+'IaaS', 'Linux', 'Office 365', 'Containers', 'Google Workspace', 'Windows', 'Network', 'Azure AD', 'Android', 'iOS',
+'Field Controller/RTU/PLC/IED', 'Safety Instrumented System/Protection Relay', 'Control Server', 'Input/Output Server',
+'Human-Machine Interface', 'Engineering Workstation', or 'Data Historian'. Values must be enclosed in quotes if it
+contains a space. The supplied values must all be relevant to the chosen domain.
+
+This will download techniques associated with Windows, macOS, Linux, and Office 365 from the Enterprise matrix:
+
+`attackexcel seed [filename] --platformfilterin Windows macOS Linux 'Office 365'`
+
+This will download techniques associated with all platforms except PRE from the Enterprise matrix:
+
+`attackexcel seed [filename] --platformfilterout PRE`
+
+This will download techniques associated with Control Server from the ICS matrix:
+
+`attackexcel seed [filename] --domain ics-attack --platformfilterin 'Control Server'`
+
+This will download techniques associated with Android from the Mobile matrix:
+
+`attackexcel seed [filename] --domain mobile-attack --platformfilterin 'Android'`
+
+
+### Subtechniques
+By default, *seed* will include all subtechniques. You can change this behavior with the no-subtechniques flag.
+
+`attackexcel seed [filename] --no-subtechniques`
+
+## Sub-command: layer
+### Basic usage
+`attackexcel layer [input filename] [output filename]`
+
+This will open the named input file, look for a worksheet named techniques, iterate through all rows except the
+first row, and use the values in each row to generate a json file suitable for use as a layer with the ATT&CK Navigator.
+
+This tool looks for the following named column headers: techniqueID, color, enabled, score, comment. Only the values
+in those columns will be included. Technically none of them are required, but the resulting json file will not work
+with the Navigator unless at least techniqueID exists, and it won't be terribly useful if at least one of the others
+is also present. If you are using this tool, then chances are you want to include score.
+
+### Specifying a different worksheet
+If, for whatever reason, the worksheet you want to use is not named *techniques*, you can specify which sheet to use
+by using the worksheet switch.
+
+`attackexcel layer [input filename] [output filename] --worksheet "ATT&CK Techniques"`
+
+### Adding metadata
+The Navigator supports a limited amount of metadata: name, and description. These can be added to the layer file
+by using switches of the same name.
+
+`attackexcel layer [input filename] [output filename] --name "My Layer" --description "My Description"`
+
+### Specifying platforms
+If you want the Navigator to only show a certain list of platforms, you can specify that at the command line as well
+using the same switches as *seed*. Note, however, that all the techniques will still be in the layer file and
+will be available within the Navigator. They will merely be hidden.
+
+`attackexcel layer [input filename] [output filename] --platformfilterin Windows MacOS Linux IaaS`
+`attackexcel layer [input filename] [output filename] --platformfilterout PRE`
+
+
--- attackexcel.py
+++ attackexcel.py
@@ -20,12 +20,13 @@ __status__ = "Prototype"
 
 default_layer_template = {
 	"versions": {
-		"attack": "8",
-		"navigator": "4.2",
-		"layer": "4.1"
+		"attack": "9",
+		"navigator": "4.3",
+		"layer": "4.2"
 	},
-	"domain": "enterprise-attack",
+	"domain": "",
 	"description": "",
+	"filters": {"platforms": []},
 	"sorting": 0,
 	"layout": {
 		"layout": "side",
@@ -50,6 +51,71 @@ default_layer_template = {
 	"selectSubtechniquesWithParent": "false"
 }
 
+valid_enterprise_platforms = {'SaaS', 'macOS', 'PRE', 'IaaS', 'Linux', 'Office 365', 'Containers',
+                              'Google Workspace', 'Windows', 'Network', 'Azure AD'}
+valid_mobile_platforms = {'Android', 'iOS'}
+valid_ics_platforms = {'Field Controller/RTU/PLC/IED', 'Safety Instrumented System/Protection Relay', 'Control Server',
+                       'Input/Output Server', 'Windows', 'Human-Machine Interface', 'Engineering Workstation',
+                       'Data Historian'}
+
+
+def validate_platform_filters_to_domain(args):
+    """
+    Since argparse does not have any ability to ensure that certain combinations of parameters are valid, this
+    function does just that.
+    """
+    platform_filter = None
+    if args.platformfilterin is not None:
+        platform_filter = args.platformfilterin
+    if args.platformfilterout is not None:
+        platform_filter = args.platformfilterout
+    if platform_filter is None:
+        return True
+    if args.domain == 'enterprise-attack':
+        for platform in platform_filter:
+            if platform not in valid_enterprise_platforms:
+                print(f'{platform} is not a valid ATT&CK platform for the Enterprise domain')
+                return False
+    elif args.domain == 'mobile-attack':
+        for platform in platform_filter:
+            if platform not in valid_mobile_platforms:
+                print(f'{platform} is not a valid ATT&CK platform for the Mobile domain')
+                return False
+    elif args.domain == 'ics-attack':
+        for platform in platform_filter:
+            if platform not in valid_ics_platforms:
+                print(f'{platform} is not a valid ATT&CK platform for the ICS domain')
+                return False
+    return True
+
+
+def create_platform_filter(args):
+    """
+    Creates an appropriate filter depending on the chose domain and filter parameters.
+    """
+    platform_filter = set()
+    if args.domain == 'enterprise-attack':
+        platform_filter = valid_enterprise_platforms
+        if args.platformfilterin is not None:
+            platform_filter = set(args.platformfilterin)
+        if args.platformfilterout is not None:
+            platform_filter = valid_enterprise_platforms.difference(set(args.platformfilterout))
+    elif args.domain == 'mobile-attack':
+        platform_filter = valid_mobile_platforms
+        if args.platformfilterin is not None:
+            platform_filter = set(args.platformfilterin)
+        if args.platformfilterout is not None:
+            platform_filter = valid_mobile_platforms.difference(set(args.platformfilterout))
+    elif args.domain == 'ics-attack':
+        platform_filter = valid_ics_platforms
+        if args.platformfilterin is not None:
+            platform_filter = set(args.platformfilterin)
+        if args.platformfilterout is not None:
+            platform_filter = valid_ics_platforms.difference(set(args.platformfilterout))
+
+    # We needed to start out with a set to do some set math, but now we want a list
+    return platform_filter
+
 
 def seed(args):
     """
@@ -61,14 +127,13 @@ def seed(args):
 
     # Get the ATT&CK techniques of the specified domain
     client = attack_client()
+    techniques = dict()
     if args.domain == 'enterprise-attack':
         techniques = client.get_enterprise_techniques(stix_format=False)
     elif args.domain == 'mobile-attack':
         techniques = client.get_mobile_techniques(stix_format=False)
     elif args.domain == 'ics-attack':
         techniques = client.get_ics_techniques(stix_format=False)
-    else:
-        techniques = dict()
 
     # Create a new workbook and some sheets
     workbook = Workbook()
@@ -79,8 +144,9 @@ def seed(args):
     # Create headers
     sheet1.cell(row=1, column=1, value='techniqueID')
     sheet1.cell(row=1, column=2, value='name')
-    sheet1.cell(row=1, column=3, value='numberOfDataSources')
-    sheet1.cell(row=1, column=4, value='description')
+    sheet1.cell(row=1, column=3, value='isSubtechnique')
+    sheet1.cell(row=1, column=4, value='platforms')
+    sheet1.cell(row=1, column=5, value='description')
     sheet2.cell(row=1, column=1, value='techniqueID')
     sheet2.cell(row=1, column=2, value='dataSourceName')
     sheet3.cell(row=1, column=1, value='dataSourceName')
@@ -91,18 +157,35 @@ def seed(args):
     # Loop through all the techniques
     sheet1_row = 2
     sheet2_row = 2
+    platform_filter = create_platform_filter(args)
     for technique in techniques:
+        # skip this technique if it was revoked
+        if ('revoked' in technique) and (technique['revoked']):
+            print(f'Skipping {technique["technique_id"]} because it was revoked.')
+            continue
+        # skip this technique if it's a subtechnique and the no-subtechniques flag is set
+        if technique['x_mitre_is_subtechnique'] and not args.subtechniques:
+            print(f'Skipping {technique["technique_id"]} because it is a subtechnique')
+            continue
+        # skip this technique if the none of associated platforms are in
+        # our platform filter i.e. the two sets are disjoint.
+        if platform_filter.isdisjoint(set(technique['platform'])):
+            print(f'Skipping {technique["technique_id"]} because none of its platforms {technique["platform"]} '
+                  f'match filter.')
+            continue
+
         sheet1.cell(row=sheet1_row, column=1, value=technique['technique_id'])
         sheet1.cell(row=sheet1_row, column=2, value=technique['technique'])
+        sheet1.cell(row=sheet1_row, column=3, value=str(technique['x_mitre_is_subtechnique']))
+        sheet1.cell(row=sheet1_row, column=4, value=str(technique['platform']))
         if 'data_sources' in technique:
-            sheet1.cell(row=sheet1_row, column=3, value=len(technique['data_sources']))
             for data_source in technique['data_sources']:
                 sheet2.cell(row=sheet2_row, column=1, value=technique['technique_id'])
                 sheet2.cell(row=sheet2_row, column=2, value=data_source)
                 data_sources.add(data_source)
                 sheet2_row = sheet2_row + 1
         if 'technique_description' in technique:
-            sheet1.cell(row=sheet1_row, column=4, value=technique['technique_description'])
+            sheet1.cell(row=sheet1_row, column=5, value=technique['technique_description'])
         sheet1_row = sheet1_row + 1
 
     # Loop through the extracted data sources
@@ -113,7 +196,7 @@ def seed(args):
 
     workbook.save(args.outfile)
 
-    print(f'Excel workbook created at \'{args.outfile}\' with {len(techniques)} techniques.')
+    print(f'Excel workbook created at \'{args.outfile}\' with {sheet1_row-1} techniques.')
 
 
 def layer(args):
@@ -126,7 +209,7 @@ def layer(args):
 
     # Load the workbook and the sheet
     workbook = load_workbook(filename=args.infile, read_only=True, data_only=True)
-    worksheet = workbook[args.sheet]
+    worksheet = workbook[args.worksheet]
 
     # Look for the following column headers and create a mapping to their column numbers:
     # techniqueID, color, enabled, score, comment
@@ -150,8 +233,10 @@ def layer(args):
     # add the name, description, and list of techniques to our template
     layer_template = default_layer_template
     layer_template['name'] = args.name
+    layer_template['domain'] = args.domain
     layer_template['description'] = args.description
     layer_template['techniques'] = techniques
+    layer_template['filters']['platforms'] = list(create_platform_filter(args))
 
     # dump the layer template to a file
     f = open(args.outfile, 'w')
@@ -177,27 +262,48 @@ def main():
     parser_seed.add_argument('--domain', type=str, choices=['enterprise-attack', 'mobile-attack', 'ics-attack'],
                              default='enterprise-attack',
                              help='the ATT&CK domain to download (default=enterprise-attack)')
+    parser_seed.add_argument('--no-subtechniques', dest='subtechniques', action='store_false')
+    parser_seed.set_defaults(subtechniques=True)
+    parser_seed_group_filter = parser_seed.add_mutually_exclusive_group()
+    parser_seed_group_filter.add_argument('--platformfilterout', type=str, nargs='+',
+                                   choices=list(valid_enterprise_platforms))
+    parser_seed_group_filter.add_argument('--platformfilterin', type=str, nargs='+',
+                                   choices=set().union(valid_enterprise_platforms,
+                                                       valid_mobile_platforms,
+                                                       valid_ics_platforms))
     parser_seed.set_defaults(func=seed)
 
     # create subparser for the 'layer' command
     parser_layer = subparsers.add_parser(name='layer', description=layer.__doc__)
-    parser_layer.add_argument('infile', type=str, help='the path to the Excel workbook.')
-    parser_layer.add_argument('sheet', type=str, help='the name of the worksheet within the identified workbook')
+    parser_layer.add_argument('infile', type=str, help='the path to the Excel workbook')
     parser_layer.add_argument('outfile', type=str, help='the path to the json document to be created')
+    parser_layer.add_argument('--worksheet', type=str, default='techniques',
+                              help='the name of the worksheet within the identified workbook')
     parser_layer.add_argument('--domain', type=str, choices=['enterprise-attack', 'mobile-attack', 'ics-attack'],
+                              default='enterprise-attack',
                               help='the ATT&CK domain of the layer to create (default=\'enterprise-attack\')')
-    parser_layer.add_argument('--name', type=str, default='attackexcel Layer',
-                              help='name of the layer (default=\'attackexcel Layer\')')
+    parser_layer.add_argument('--name', type=str, default='Attackexcel Layer',
+                              help='name of the layer (default=\'Attackexcel Layer\')')
     parser_layer.add_argument('--description', type=str, default='',
                               help='a description for the layer (default=none)')
+    parser_layer_group = parser_layer.add_mutually_exclusive_group()
+    parser_layer_group.add_argument('--platformfilterout', type=str, nargs='+',
+                                    choices=list(valid_enterprise_platforms))
+    parser_layer_group.add_argument('--platformfilterin', type=str, nargs='+',
+                                    choices=set().union(valid_enterprise_platforms,
+                                                        valid_mobile_platforms,
+                                                        valid_ics_platforms))
     parser_layer.set_defaults(func=layer)
 
-    # parse the arguments and call the function to handle the appropriate command
+    # parse the arguments
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     args = parser.parse_args()
-    args.func(args)
+
+    # validate domain to platform match, and call the function to handle the appropriate command
+    if validate_platform_filters_to_domain(args):
+        args.func(args)
 
 
 if __name__ == '__main__':
